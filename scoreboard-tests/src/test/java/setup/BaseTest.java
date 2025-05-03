6b8058a15c6a7bcdd320a16563d7f701031fa1e4
vignesh-kg <vigneshkg515@gmail.com>
2025-05-03 20:34:53 +0300

    implemented BaseTest

--- scoreboard-tests/src/test/java/setup/BaseTest.java
+++ scoreboard-tests/src/test/java/setup/BaseTest.java
@@ -4,8 +4,10 @@ import com.scoreboard.config.ServicesConfig;
 import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 
 @SpringBootTest(classes = {ServicesConfig.class})
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@DirtiesContext
 public abstract class BaseTest {
 }
